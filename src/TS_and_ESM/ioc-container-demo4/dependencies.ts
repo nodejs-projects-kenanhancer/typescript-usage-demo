2f8d74f19bd61912c9f07bfe696a9c826e0fc91b
kenan <kenan@10dot1.co.uk>
2023-01-21 15:52:15 +0000

    ok

--- src/TS_and_ESM/ioc-container-demo4/dependencies.ts
+++ src/TS_and_ESM/ioc-container-demo4/dependencies.ts
@@ -1,4 +1,4 @@
-import { GreetingService } from "/Users/kenanhancer/Documents/projects/nodejs/typescript/typescript-usage-demo/src/TS_and_ESM/ioc-container-demo4/src/greeting-service";
-import { Helper } from "/Users/kenanhancer/Documents/projects/nodejs/typescript/typescript-usage-demo/src/TS_and_ESM/ioc-container-demo4/src/helper";
+import { GreetingService } from "./src/greeting-service";
+import { Helper } from "./src/helper";
 
 export { GreetingService, Helper };
