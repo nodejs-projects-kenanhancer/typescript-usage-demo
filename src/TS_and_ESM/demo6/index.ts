15531311c37e181473c0b695140c811ff12c4935
kenanhancer <kenanhancer@gmail.com>
2023-01-30 11:49:27 +0000

    ok

--- src/TS_and_ESM/demo6/index.ts
+++ src/TS_and_ESM/demo6/index.ts
@@ -1,5 +1,5 @@
 const addAward = {
-  path: `/project-v2/:projectId/award/:opportunityId/:opportunityLotId/add/:awardType`,
+  path: `/project-v2/:projectId/award/:opportunityId/:opportunityLotId/add/:awardType` as const,
   title: "Add an award",
 };
 
@@ -16,32 +16,65 @@ type ExtractRouteParams<Path> = Path extends `${infer PartA}/${infer PartB}`
   ? ExtractParam<Path>
   : never;
 
-type T3 =
-  ExtractRouteParams<"/project-v2/:projectId/award/:opportunityId/:opportunityLotId/edit/:awardType">;
+type RouteParams<Path> = Record<ExtractRouteParams<Path>, string>;
 
-type T4 = Record<T3, string>;
+type T3 = RouteParams<typeof addAward.path>;
 
-const a1: T4 = {
+const a1: T3 = {
   awardType: "",
   opportunityId: "",
   opportunityLotId: "",
   projectId: "",
 };
 
-// export type ExtractRouteParams<
-//   T extends string,
-//   U = string | number | boolean
-// > = string extends T
-//   ? { [k in string]?: U }
-//   : T extends `${infer _Start}:${infer ParamWithOptionalRegExp}/${infer Rest}`
-//   ? ParamWithOptionalRegExp extends `${infer Param}(${infer _RegExp})`
-//     ? ExtractRouteOptionalParam<Param, U> & ExtractRouteParams<Rest, U>
-//     : ExtractRouteOptionalParam<ParamWithOptionalRegExp, U> &
-//         ExtractRouteParams<Rest, U>
-//   : T extends `${infer _Start}:${infer ParamWithOptionalRegExp}`
-//   ? ParamWithOptionalRegExp extends `${infer Param}(${infer _RegExp})`
-//     ? ExtractRouteOptionalParam<Param, U>
-//     : ExtractRouteOptionalParam<ParamWithOptionalRegExp, U>
-//   : {};
+const projectDashboardV2Routes = [
+  {
+    path: "/project-v2/:projectId/project-overview",
+    title: "Project Overview",
+    // component: ProjectDashboard,
+    exact: true,
+    isPrivate: true,
+    inSideBar: true,
+  },
+  {
+    path: "/project-v2/:projectId/tasks",
+    title: "Tasks",
+    // component: TaskWrapper,
+    exact: true,
+    isPrivate: true,
+    inSideBar: true,
+  },
+  {
+    path: "/project-v2/:projectId/project-inbox",
+    title: "Project inbox",
+    // component: ProjectInboxContainer,
+    exact: true,
+    isPrivate: true,
+    inSideBar: true,
+  },
+] as const;
+
+type CamelToSnake<T extends string> = string extends T
+  ? string
+  : T extends `${infer C0}${infer R}`
+  ? `${C0 extends " " ? "_" : ""}${Uppercase<
+      Exclude<C0, " "> extends never ? "" : C0
+    >}${CamelToSnake<R>}`
+  : "";
+
+type T6 = CamelToSnake<"Project inbox">;
+
+type T7 = CamelToSnake<"Project Inbox">;
+
+type T8 = typeof projectDashboardV2Routes[number];
+
+type T9<T extends T8> = T extends any ? Pick<T, "path" | "title"> : never;
+
+type T10 = T9<typeof projectDashboardV2Routes[number]>;
+
+const a10: T10 = {
+  path: "/project-v2/:projectId/project-overview",
+  title: "Project Overview",
+};
 
 export {};
