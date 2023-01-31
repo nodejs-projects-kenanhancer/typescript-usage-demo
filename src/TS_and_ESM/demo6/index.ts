d8d0c00ae10a1adcfb8f9b3e27a34856807f0325
kenanhancer <kenanhancer@gmail.com>
2023-01-31 00:21:51 +0000

    ok

--- src/TS_and_ESM/demo6/index.ts
+++ src/TS_and_ESM/demo6/index.ts
@@ -1,12 +1,48 @@
-const addAward = {
-  path: `/project-v2/:projectId/award/:opportunityId/:opportunityLotId/add/:awardType`,
-  title: "Add an award",
-} as const;
+const projectDashboardV2AwardsRoute = [
+  {
+    path: `/project-v2/:projectId/award/:opportunityId/:opportunityLotId/add/:awardType`,
+    title: "Add an award",
+    exact: true,
+    isPrivate: true,
+  },
+  {
+    path: `/project-v2/:projectId/award/:opportunityId/:opportunityLotId/edit/:awardType`,
+    title: "Edit an award",
+    exact: true,
+    isPrivate: true,
+  },
+  {
+    path: "/project-v2/:projectId/project-overview",
+    title: "Project Overview",
+    exact: true,
+    isPrivate: true,
+    inSideBar: true,
+  },
+] as const;
 
-const editAward = {
-  path: `/project-v2/:projectId/award/:opportunityId/:opportunityLotId/edit/:awardType`,
-  title: "Edit an award",
-} as const;
+const projectDashboardV2Routes = [
+  {
+    path: "/project-v2/:projectId/project-overview",
+    title: "Project Overview",
+    exact: true,
+    isPrivate: true,
+    inSideBar: true,
+  },
+  {
+    path: "/project-v2/:projectId/tasks",
+    title: "Tasks",
+    exact: true,
+    isPrivate: true,
+    inSideBar: true,
+  },
+  {
+    path: "/project-v2/:projectId/project-inbox",
+    title: "Project inbox",
+    exact: true,
+    isPrivate: true,
+    inSideBar: true,
+  },
+] as const;
 
 type ExtractParam<Path> = Path extends `:${infer A}` ? A : never;
 
@@ -18,42 +54,6 @@ type ExtractRouteParams<Path> = Path extends `${infer PartA}/${infer PartB}`
 
 type RouteParams<Path> = Record<ExtractRouteParams<Path>, string>;
 
-type T3 = RouteParams<typeof addAward.path>;
-
-const a1: T3 = {
-  awardType: "",
-  opportunityId: "",
-  opportunityLotId: "",
-  projectId: "",
-};
-
-const projectDashboardV2Routes = [
-  {
-    path: "/project-v2/:projectId/project-overview",
-    title: "Project Overview",
-    // component: ProjectDashboard,
-    exact: true,
-    isPrivate: true,
-    inSideBar: true,
-  },
-  {
-    path: "/project-v2/:projectId/tasks",
-    title: "Tasks",
-    // component: TaskWrapper,
-    exact: true,
-    isPrivate: true,
-    inSideBar: true,
-  },
-  {
-    path: "/project-v2/:projectId/project-inbox",
-    title: "Project inbox",
-    // component: ProjectInboxContainer,
-    exact: true,
-    isPrivate: true,
-    inSideBar: true,
-  },
-] as const;
-
 type CamelToSnake<T extends string> = string extends T
   ? string
   : T extends `${infer C0}${infer R}`
@@ -62,19 +62,72 @@ type CamelToSnake<T extends string> = string extends T
     >}${CamelToSnake<R>}`
   : "";
 
+type T3 =
+  RouteParams<"/project-v2/:projectId/award/:opportunityId/:opportunityLotId/add/:awardType">;
+
+const a1: T3 = {
+  awardType: "",
+  opportunityId: "",
+  opportunityLotId: "",
+  projectId: "",
+};
+
 type T6 = CamelToSnake<"Project inbox">;
 
 type T7 = CamelToSnake<"Project Inbox">;
 
 type T8 = typeof projectDashboardV2Routes[number];
 
-type T9<T extends T8> = T extends any ? Pick<T, "path" | "title"> : never;
+type GenerateRoute<T extends { path: string; title: string }> = T extends {
+  path: infer TP extends string;
+  title: infer TT extends string;
+}
+  ? { [P in CamelToSnake<TT>]: TP }
+  : never;
 
-type T10 = T9<typeof projectDashboardV2Routes[number]>;
+type T11<T extends { path: string; title: string }> = T extends {
+  path: infer TP extends string;
+  title: infer TT extends string;
+}
+  ? CamelToSnake<TT>
+  : never;
 
-const a10: T10 = {
-  path: "/project-v2/:projectId/project-overview",
-  title: "Project Overview",
+type UnionToIntersection<U> = (
+  U extends any ? (arg: U) => any : never
+) extends (arg: infer I) => void
+  ? I
+  : never;
+
+type T10 = UnionToIntersection<
+  GenerateRoute<typeof projectDashboardV2AwardsRoute[number]>
+>;
+
+const ll: T10 = {
+  ADD_AN_AWARD:
+    "/project-v2/:projectId/award/:opportunityId/:opportunityLotId/add/:awardType",
+  EDIT_AN_AWARD:
+    "/project-v2/:projectId/award/:opportunityId/:opportunityLotId/edit/:awardType",
+  PROJECT_OVERVIEW: "/project-v2/:projectId/project-overview",
 };
+
+type T12 = T11<typeof projectDashboardV2AwardsRoute[number]>;
+
+const route = <
+  T extends keyof T10,
+  K extends RouteParams<T10[T]>,
+  L extends T10[T]
+>(
+  name: T,
+  value: K
+) => {};
+
+route("ADD_AN_AWARD", {
+  awardType: "",
+  opportunityId: "",
+  opportunityLotId: "",
+  projectId: "",
+});
+
+route("PROJECT_OVERVIEW", { projectId: "" });
 
 export {};
